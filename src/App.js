3d767f468edfbcb5a9a68fda526e9053481c39f2
ChrisJRamirez <chrisjamesramirez@gmail.com>
2021-07-18 19:47:20 -0400

    beginning app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React from "react";
+import './App.css';
+import Axios from "axios"
 // setting up codegrade
 
+const initialFormValues = {
+  //text inputs
+  name: "",
+  size: "",
+  //booleans & checkboxes
+  topping1: false,
+  topping2: false,
+
+  special: "",
+ 
+}
+
+const initialFormErrors = {
+  name: "",
+  size: "",
+  
+}
+
+const initialOrders = []
+const initialDisabled = true
+
 const App = () => {
+  const [orders, setOrders] = useState(initialOrders)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
+
+
+
+
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <p>Welcome to Lambda Eats! Please place your order and let us know if
+         you have any special instructions!</p>
     </>
   );
 };
